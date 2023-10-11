a1b5ed9818c4877303fb3cd72064444eb0a7197b
emmanuel sheshi <56793157+emmanuelsheshi@users.noreply.github.com>
2023-10-11 17:09:06 +0100

    Update homePage.js
    done home

--- routes/homePage.js
+++ routes/homePage.js
@@ -1,34 +1,34 @@
 import { Router } from 'express'
 var router = Router()
 import { parse } from 'url'
-// import firebase from 'firebase'
 
-// import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 /* GET home page. */
 router.use('/', function (req, res, next) {
-  // const { query } = parse(req.url, true)
-  // const auth = getAuth()
+  const { query } = parse(req.url, true)
+  const auth = getAuth()
 
-  // onAuthStateChanged(auth, (user) => {
-  //   if (user) {
-  //     // User is signed in, see docs for a list of available properties
-  //     // https://firebase.google.com/docs/reference/js/auth.user
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
 
-  //     const uid = user.uid
-  //     console.log('your have been authenticated')
+      const uid = user.uid
+      console.log('your have been authenticated')
 
-  //     res.render('homePage', {
-  //       title: 'imperiumPMS',
-  //       username: query.username,
-  //     })
-  //     // ...
-  //   } else {
-  //     // User is signed out
-  //     console.log('you have to sign in')
-  //     res.redirect('/index')
-  //   }
-  // })
+      res.render('homePage', {
+        title: 'imperiumPMS',
+        username: query.username,
+      })
+      // ...
+    } else {
+      // User is signed out
+      console.log('you have to sign in')
+      res.redirect('/index')
+    }
+  })
 })
 
 export default router
